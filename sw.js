fdd40bebeda7d6b3f78f2d74ffb8850e03085343
pranavparekhcontent <pranavparekhcontent@gmail.com>
2026-08-15 01:53:17 +0530

    chore: bump version to 2.9.7 and sw cache to v117

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first for app shell, network-first for API
  */
 
-const CACHE_NAME = 'acad-file-v116';
+const CACHE_NAME = 'acad-file-v117';
 const APP_SHELL = [
   './',
   './index.html',
