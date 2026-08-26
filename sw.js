50cd7ab7b07d6e4f404df8857b841f965eec8cd6
pranavparekhcontent <pranavparekhcontent@gmail.com>
2026-08-26 21:07:02 +0530

    Bump version to 3.1.27 (build 158)

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
  * Cache-first for app shell, network-first for API
  */
 
-const CACHE_NAME = 'acad-file-v157';
-const DATA_CACHE_NAME = 'acad-data-v157';
+const CACHE_NAME = 'acad-file-v158';
+const DATA_CACHE_NAME = 'acad-data-v158';
 const APP_SHELL = [
   './',
   './index.html',
